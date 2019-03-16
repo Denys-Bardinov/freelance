3fbe9020390aba7f1675f165b12823d28b7e1771
denblind0404 <40865458+denblind0404@users.noreply.github.com>
2019-03-16 09:10:05 +0200

    Add files via upload

---
+++ freelance/js/custom.js
@@ -0,0 +1,8 @@
+jQuery(function ($) {
+$(function () {
+                $('.burger_trigger').click(function () {
+                    $('.menu-list').toggleClass('menu_opened');
+                });
+            });
+});
+
